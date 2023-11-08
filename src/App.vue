060720dea404eb2dd830efc20de2de87826a73f2
James Shaw <jd3shaw@gmail.com>
2023-11-08 11:42:01 +1100

    svelte stuff

---
+++ src/App.vue
@@ -0,0 +1,129 @@
+<template>
+  <div class="app">
+    <div>
+      <a class="form" href="https://forms.gle/FV1nwewC7VyRKzf58" target="_blank">BajoSmells Shirts ROI</a>
+    </div>
+    <div @click="playVideo" class="background">
+      <img src="bajosmells.jpg" alt="Background Image" />
+    </div>
+    <div class="click-text" @click="handleClick">
+      Clickies
+    </div>
+    <video v-if="showVideo" autoplay @ended="stopVideo" ref="videoElement">
+      <source src="bajosmells.mp4" type="video/mp4" />
+    </video>
+    <div>
+      <a class="twitch-link" href="https://www.twitch.tv/bajo" target="_blank">Visit Bajo's Twitch Channel</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showVideo: false,
+    };
+  },
+  methods: {
+    playVideo() {
+      this.showVideo = true;
+      const video = this.$refs.videoElement;
+      if (video) {
+        video.play();
+      }
+    },
+    stopVideo() {
+      this.showVideo = false;
+      const video = this.$refs.videoElement;
+      if (video) {
+        video.pause();
+        video.currentTime = 0;
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.app {
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  margin: 0;
+  padding: 0;
+}
+
+.background {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  overflow: hidden;
+}
+
+img {
+  max-width: 100%;
+  max-height: 100%;
+  object-fit: contain;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+.click-text {
+  position: fixed;
+  top: 45%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background: rgba(0, 0, 0, 0.7);
+  color: white;
+  padding: 5px 5px;
+  font-size: 12px;
+  z-index: 2; /* Set a high z-index to ensure it appears on top of everything else */
+  pointer-events: none;
+}
+
+video {
+  max-width: 90%;
+  max-height: 80%;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 4;
+}
+
+.form {
+  position: absolute;
+  top: 0px;
+  width: 100%;
+  text-align: center;
+  background: rgba(0, 0, 0, 0.7);
+  color: white;
+  font-size: 32px;
+  display: inline-block;
+  text-decoration: none;
+  padding: 10px 20px;
+  z-index: 1;
+}
+.form:hover {
+  color: white;
+}
+
+.twitch-link {
+  position: absolute;
+  bottom: 0px;
+  width: 100%;
+  text-align: center;
+  background: rgba(0, 0, 0, 0.7);
+  color: white;
+  font-size: 32px;
+  display: inline-block;
+  text-decoration: none;
+  padding: 10px 20px;
+  z-index: 1;
+}
+.twitch-link:hover {
+  color: white;
+}
+</style>
