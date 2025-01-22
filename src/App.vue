289e427df7de12dc67eb2f5b9ac61cbf2890b354
James Shaw <jd3shaw+aws@gmail.com>
2025-01-22 21:14:41 +1100

    Replace router-link with a standard anchor tag for He Also Has Worms in App.vue

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@
       <source src="bajosmells.mp4" type="video/mp4" />
     </video>
     <div>
-      <router-link to="/he/also/has/worms" class="form">He Also Has Worms</router-link>
+      <a href="/he/also/has/worms" class="form">He Also Has Worms</a>
     </div>
   </div>
 </template>
